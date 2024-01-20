2f48449b494ecf794f6b3dceea0c9a6a48239ef7
slye20 <samuellim2011@gmail.com>
2024-01-20 18:26:15 +0800

    Updated gui and edited state

--- src/components/CreateItem.jsx
+++ src/components/CreateItem.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateItem = () => {
+  const [title, setShowTitle] = useState("");
+  const [date, setDate] = useState("2024-01-01");
+  const [time, setTime] = useState("00:00");
+  const [frequency, setFrequency] = useState(1);
+  const [message, setMessage] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="flex flex-col justify-between p-4 leading-normal border rounded-lg border-gray-200 hover:bg-gray-100 w-4/5">
+    <form
+      onSubmit={onSubmit}
+      className="flex flex-col justify-between p-4 leading-normal border rounded-lg border-gray-200 shadow-2xl w-4/5 "
+    >
       <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
         New Sheduler
       </h5>
@@ -17,45 +30,56 @@ const CreateItem = () => {
         placeholder="Title"
         aria-label="Title"
         name="Title"
-      />
-
-      {/* Time */}
-      <label for="countries" className="block mt-2 text-sm font-medium">
-        Time
-      </label>
-
-      {/* Date */}
-      <input
-        type="date"
-        id="birthday"
-        name="birthday"
-        className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 focus:bg-white"
-      ></input>
-
-      {/* Time */}
-      <input
-        type="time"
-        id="appt"
-        name="appt"
-        min="09:00"
-        max="18:00"
-        className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 focus:bg-white"
+        onChange={(e) => setShowTitle(e.target.value)}
+        value={title}
         required
       />
 
-      {/* Frequency */}
-      <select
-        id="frequency"
-        className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 focus:bg-white"
-      >
-        <option selected value="1">
-          Doesn't Repeat
-        </option>
-        <option value="2">Daily</option>
-        <option value="3">Weekly</option>
-        <option value="4">Monthly</option>
-        <option value="5">Annually</option>
-      </select>
+      {/* Label */}
+      <label for="countries" className="block mt-2 text-sm font-medium  ">
+        Time
+      </label>
+
+      <div className="flex flex-row">
+        {/* Date */}
+        <input
+          type="date"
+          id="birthday"
+          name="birthday"
+          className="flex-1 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 focus:bg-white"
+          value={date}
+          onChange={(x) => setDate(x.target.value)}
+          required
+        ></input>
+
+        {/* Time */}
+        <input
+          type="time"
+          id="appt"
+          name="appt"
+          className="flex-1 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 focus:bg-white"
+          required
+          onChange={(x) => setTime(x.target.value)}
+          value={time}
+        />
+
+        {/* Frequency */}
+        <select
+          required
+          id="frequency"
+          className="flex-1 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 focus:bg-white"
+          value={frequency}
+          onChange={(x) => setFrequency(x.target.value)}
+        >
+          <option selected value="1">
+            Doesn't Repeat
+          </option>
+          <option value="2">Daily</option>
+          <option value="3">Weekly</option>
+          <option value="4">Monthly</option>
+          <option value="5">Annually</option>
+        </select>
+      </div>
 
       {/* Message */}
       <label for="message" className="block mt-2 text-sm font-medium">
@@ -66,15 +90,18 @@ const CreateItem = () => {
         rows="4"
         className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 focus:bg-white"
         placeholder="Write your thoughts here..."
+        required
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
       ></textarea>
 
       <button
-        type="button"
-        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 mt-4"
+        type="submit"
+        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 mt-4"
       >
         submit
       </button>
-    </div>
+    </form>
   );
 };
 
